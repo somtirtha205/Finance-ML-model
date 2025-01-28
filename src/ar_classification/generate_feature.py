abcaaec931fc46d4ee4c2367feafa762c9e1b474
somtirtha205 <somtirtha@yahoo.com>
2025-01-28 09:57:59 -0500

    All files to branch1

---
+++ src/ar_classification/generate_feature.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+
+def get_features(X, X_train):
+    customer_features = X_train.groupby('customerID').agg(
+    {
+    'InvoiceDate': ['count'],
+    'InvoiceAmount': ['sum', 'mean', 'max'],
+    'Disputed': ['mean'],
+    'PaperlessBill': ['sum', 'mean'],
+    'DaysToSettle': ['mean', 'max'],
+    'DaysLate': ['mean', 'max'],
+    }).reset_index()
+
+
+    # Pandas group-by creates a MultiIndex, which we don't want. The following few lines
+    # will rename the columns of the dataframe to something more reasonable.
+    customer_features.columns = ["_".join(x) for x in customer_features.columns.ravel()]
+    customer_features = customer_features.rename(columns={'customerID_': "customerID"})
+    customer_features.info()
+
+    cat_feature_names = ['countryCode', 'InvoiceDate_DOW', 'InvoiceDate_Month', 'DueDate_DOW', 'DueDate_Month']
+
+    # Fit the OneHotEncoder and save the `ohe` object for later.
+    ohe = OneHotEncoder(categories='auto', handle_unknown='ignore', sparse_output=False)
+    ohe = ohe.fit(X_train[cat_feature_names])
+    ohe_feature_names = list(ohe.get_feature_names_out(cat_feature_names))
+
+    #print("\n\nNames of {} OHE features:".format(len(ohe_feature_names)))
+    #print(ohe_feature_names)
+
+    # Use the OHE to get numerical features from categorical
+    ohe_features = ohe.transform(X[cat_feature_names])
+    ohe_features_df = pd.DataFrame(ohe_features, columns=ohe_feature_names)
+
+    X_features = X.merge(customer_features, how="left", on="customerID").reset_index(drop=True)
+
+    X_features = X_features.drop(['customerID', 'InvoiceDate', 'countryCode', 'InvoiceDate_DOW', 'InvoiceDate_Month', 
+                                  'DueDate_DOW', 'DueDate_Month', 'Disputed', 'SettledDate', 'DaysToSettle', 'DaysLate',
+                                  'DueDate', 'PaperlessDate'], axis=1)
+
+    features_all = pd.concat([X_features, ohe_features_df], axis=1)
+
+    return features_all
---
+++ src/ar_classification/model_build_and_evaluate.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
+from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+def train_and_evaluate_model(X_train, X_test, y_train, y_test):
+
+    tscv = TimeSeriesSplit()
+
+    pipeline1 = make_pipeline(StandardScaler(), LogisticRegression(class_weight = "balanced", random_state=0))
+
+    param_grid1 = {
+        'standardscaler__with_mean' : [True, False],
+        'logisticregression__C' : [0.1, 0.5, 1],
+    }
+
+    search1 = GridSearchCV(pipeline1, param_grid1, cv=tscv, n_jobs=5, scoring='f1_micro', return_train_score=True, 
+                           verbose=3)
+
+    search1 = search1.fit(X_train, y_train)
+
+    y_pred = search1.predict(X_test)
+
+    print("F1 Score   = {:.3f}".format(f1_score(y_test, y_pred, average="micro")))
+
+    print("\nConfusion Matrix:")
+    unique_label = np.unique([y_test, y_pred])
+    cm = pd.DataFrame(
+    confusion_matrix(y_test, y_pred, labels=unique_label), 
+    index=['true:{:}'.format(x) for x in unique_label], 
+    columns=['pred:{:}'.format(x) for x in unique_label])
+    print(cm)
+
+    print("\nClassification Report:")
+    print(classification_report(y_test, y_pred)) 
+
+    return search1
