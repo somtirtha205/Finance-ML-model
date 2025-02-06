4b3a64aedb585e56e82bfb11b5f129990a4accc9
somtirtha205 <somtirtha@yahoo.com>
2025-02-05 19:29:51 -0500

    change in scripts

--- scripts/deploy_model.py
+++ scripts/deploy_model.py
@@ -9,12 +9,15 @@ subscription_id = os.getenv("AZURE_SUBSCRIPTION_ID")
 tenant_id = os.getenv("AZURE_Tenant_ID")
 sp_id = os.getenv("AZURE_SP_ID")
 sp_secret = os.getenv("AZURE_SP_SECRET")
+workspace_id = os.getenv("AZURE_WORKSPACE_ID")
 
 sp = ServicePrincipalAuthentication(
     tenant_id=tenant_id, service_principal_id=sp_id, service_principal_password=sp_secret
 )
 
-ws = Workspace.get(name="WorkspaceML", resource_group="ResourceGroupML", subscription_id=subscription_id, auth=sp)
+ws = Workspace.get(
+    name="WorkspaceML", auth=sp, subscription_id=subscription_id, resource_group="ResourceGroupML", id=workspace_id
+)
 
 # Get the registered model
 model = Model(ws, name="AR_model")
