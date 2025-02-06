bfdc83b9e3d9309a49e7db302e1a60e58ecc430f
somtirtha205 <somtirtha@yahoo.com>
2025-02-06 11:33:25 -0500

    change

--- scripts/register_model.py
+++ scripts/register_model.py
@@ -8,10 +8,8 @@ tenant_id = os.getenv("AZURE_Tenant_ID")
 sp_id = os.getenv("AZURE_SP_ID")
 sp_secret = os.getenv("AZURE_SP_SECRET")
 
-authority_url = f"https://login.microsoftonline.com/{tenant_id}"
-
 sp = ServicePrincipalAuthentication(
-    tenant_id=tenant_id, service_principal_id=sp_id, service_principal_password=sp_secret, authority=authority_url
+    tenant_id=tenant_id, service_principal_id=sp_id, service_principal_password=sp_secret
 )
 
 ws = Workspace.get(name="WorkspaceML", auth=sp, subscription_id=subscription_id, resource_group="ResourceGroupML")
