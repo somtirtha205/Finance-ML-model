74a9bf81840ff27bb151065532b8b1a260b4932c
somtirtha205 <somtirtha@yahoo.com>
2025-02-06 13:13:20 -0500

    changes in model

--- scripts/deploy_model.py
+++ scripts/deploy_model.py
@@ -1,18 +1,10 @@
 import os
 
 from azureml.core import Environment, Model, Workspace
-from azureml.core.authentication import ServicePrincipalAuthentication
 from azureml.core.model import InferenceConfig
 from azureml.core.webservice import AciWebservice
 
 subscription_id = os.getenv("AZURE_SUBSCRIPTION_ID")
-tenant_id = os.getenv("AZURE_Tenant_ID")
-sp_id = os.getenv("AZURE_SP_ID")
-sp_secret = os.getenv("AZURE_SP_SECRET")
-
-sp = ServicePrincipalAuthentication(
-    tenant_id=tenant_id, service_principal_id=sp_id, service_principal_password=sp_secret
-)
 
 ws = Workspace.get(name="WorkspaceML", subscription_id=subscription_id, resource_group="ResourceGroupML")
 
--- scripts/register_model.py
+++ scripts/register_model.py
@@ -1,19 +1,13 @@
 import os
 
 from azureml.core import Model, Workspace
-from azureml.core.authentication import ServicePrincipalAuthentication
 
 subscription_id = os.getenv("AZURE_SUBSCRIPTION_ID")
-tenant_id = os.getenv("AZURE_Tenant_ID")
-sp_id = os.getenv("AZURE_SP_ID")
-sp_secret = os.getenv("AZURE_SP_SECRET")
-
-sp = ServicePrincipalAuthentication(
-    tenant_id=tenant_id, service_principal_id=sp_id, service_principal_password=sp_secret
-)
 
 ws = Workspace.get(name="WorkspaceML", subscription_id=subscription_id, resource_group="ResourceGroupML")
 
-model = Model.register(workspace=ws, model_name="AR_model", model_path="./model/ar.pkl", description="Finance ML model")
+model = Model.register(
+    workspace=ws, model_name="AR_model", model_path="./model/model.pkl", description="Finance ML model"
+)
 
 print(f"Model {model.name} registered successfully!")
